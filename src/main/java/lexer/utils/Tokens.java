c75e986f2ecd68bb592b48ac815c27dcaac1bdc0
Luigi-Crisci <luigicrisci1997@gmail.com>
2020-10-13 22:01:28 +0200

    Tokens priority

--- src/main/java/lexer/utils/Tokens.java
+++ src/main/java/lexer/utils/Tokens.java
@@ -1,24 +1,32 @@
 package lexer.utils;
 
 public enum Tokens {
-    INT,
-    FLOAT,
-    ID,
-    OP,
     //Keywords
     IF,
     THEN,
     ELSE,
     WHILE,
     FOR,
+    INT,
+    FLOAT,
+    ID,
+    OP,
     //Separators
     L_PAR,
     R_PAR,
     L_CURLY,
     R_CURLY,
     COLON,
-    S_COLON
+    S_COLON;
+
+    public static int compareTo(Tokens x,Tokens y) {
+        for(Tokens currentTokens: values()){
+            if(currentTokens.equals(x))
+                return 1;
+            if(currentTokens.equals(y))
+                return -1;
+        }
+        return 0;
+    }
 }
-
-
 
